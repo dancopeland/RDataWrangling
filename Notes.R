92c1a6e55c269db6878f3826a870a6e107f68f9a
dancopeland <daniel.i.copeland@vanderbilt.edu>
2019-05-22 12:44:19 -0400

    updated boyNames, still need to put in 1 col

--- Notes.R
+++ Notes.R
@@ -33,6 +33,54 @@ read.baby.names( boy.file.names[1])
 
 boysNames = map(boy.file.names, read.baby.names)
 
+glimpse(boysNames)
+
+col_names = colnames(boysNames[[1]])
+
+col_names
+
+boysNames[[1]] <- select(boysNames[[1]], "Name...2" , "Name...6", "Count...3", "Count...7" )
+boysNames[[1]]
+
+boysNames[[1]] <- drop_na(boysNames[[1]])
+boysNames[[1]]
+
+testBoyNames <- boysNames[[2]]
+typeof(testBoyNames)
+testBoyNames <- select(testBoyNames, str_subset(colnames(testBoyNames), "Count"), str_subset(colnames(testBoyNames), "Name"))
+
+testBoyNames
+
+condense.baby.names <- function(data.frame)  {
+  select(data.frame, str_subset(colnames(data.frame), "Count"), str_subset(colnames(data.frame), "Name"))
+}
+
+boyNames <- map(boysNames, condense.baby.names)
+
+x <- 0
+for (df in boyNames) {
+  x = x + 1
+  df <- df[,c(3,1,4,2)]
+  boyNames[[x]] <- df
+}
+
+boyNames 
+
+map(boyNames, drop_na)
+
+# bayNames <- boyNames[c(3,1,4,2)]
+# 
+# names(boyNames[[1]][1]) <- ""
+# 
+boyNames[[1]]<- boyNames[[1]][,c(3,1,4,2)]
+boyNames[[1]]
+
+
+
+
+
+
+
 
 
 
